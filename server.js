5f16c9106cfcd71d3aece9fcf853016ace865670
sarangraii <sarang2452@gmail.com>
2026-01-07 11:55:47 +0530

    DONE

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MERNPROJECTCLONE', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MERNPROJECTCLONE', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MERNPROJECTCLONE');
 
 // Request History Schema
 const requestHistorySchema = new mongoose.Schema({
